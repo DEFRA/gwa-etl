6d14c4ddf25ee34c0403d1d25a70459ad6ed9344
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-04-28 11:00:18 +0100

    refactor: reset modules and mocks for every test

--- ImportData/index.js
+++ ImportData/index.js
@@ -3,6 +3,7 @@ module.exports = async function (context) {
     // Copy file contents
     const { allUsers } = context.bindings
 
+    // TODO: set email to be id
     context.bindings.users = allUsers
   } catch (e) {
     context.log.error(e)
