09676d715ab5fae472dbfe12de5d0d11e101d129
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-05-04 10:40:19 +0100

    refactor: extract to smaller functions

--- ImportData/index.js
+++ ImportData/index.js
@@ -8,10 +8,56 @@ const client = new CosmosClient(connectionString)
 const db = client.database(dbName)
 const usersContainer = db.container(usersContainerName)
 
+async function setUsersInactive (container, emailAddresses) {
+  const usersInactive = []
+  for (const emailAddress of emailAddresses) {
+    usersInactive.push(emailAddress)
+    const { resource } = await container.item(emailAddress, emailAddress).read()
+    resource.active = false
+    await container.item(emailAddress, emailAddress).replace(resource)
+  }
+  return usersInactive
+}
+
+async function createOrUpdateUsers (container, usersToImport, existingUserIds) {
+  const usersCreated = []
+  const usersUpdated = []
+
+  const importDate = Date.now()
+  for (const user of usersToImport) {
+    const { emailAddress } = user
+    user.active = true
+    user.id = emailAddress
+    user.importDate = importDate
+    delete user.emailAddress
+
+    const { resource } = await container.item(emailAddress, emailAddress).read()
+
+    if (resource) {
+      usersUpdated.push(emailAddress)
+      await container.item(emailAddress, emailAddress).replace({ ...resource, ...user })
+      existingUserIds.delete(emailAddress)
+    } else {
+      usersCreated.push(emailAddress)
+      await container.items.create(user)
+    }
+  }
+  return { usersCreated, usersUpdated }
+}
+
+function getUsersToImport (context) {
+  const { blobContents } = context.bindings
+  return JSON.parse(blobContents)
+}
+
+async function getExistingUsers (container) {
+  const queryResponse = await container.items.query('SELECT c.id FROM c').fetchAll()
+  return new Set(queryResponse.resources.map(r => r.id))
+}
+
 module.exports = async function (context) {
   try {
-    const { blobContents } = context.bindings
-    const usersToImport = JSON.parse(blobContents)
+    const usersToImport = getUsersToImport(context)
 
     if (!usersToImport.length) {
       context.log.warn('No users to import, returning early.')
@@ -19,43 +65,12 @@ module.exports = async function (context) {
     }
     context.log(`Users to import: ${usersToImport.length}.`)
 
-    const queryResponse = await usersContainer.items.query('SELECT c.id FROM c').fetchAll()
-    const existingUserIds = new Set(queryResponse.resources.map(r => r.id))
+    const existingUserIds = await getExistingUsers(usersContainer)
     context.log(`Users already existing: ${existingUserIds.size}.`)
 
-    const usersCreated = []
-    const usersUpdated = []
-    const usersInactive = []
+    const { usersCreated, usersUpdated } = await createOrUpdateUsers(usersContainer, usersToImport, existingUserIds)
 
-    // TODO: refactor to function
-    const importDate = Date.now()
-    for (const user of usersToImport) {
-      const { emailAddress } = user
-      user.active = true
-      user.id = emailAddress
-      user.importDate = importDate
-      delete user.emailAddress
-
-      const { resource } = await usersContainer.item(emailAddress, emailAddress).read()
-
-      if (resource) {
-        usersUpdated.push(emailAddress)
-        await usersContainer.item(emailAddress, emailAddress).replace({ ...resource, ...user })
-        existingUserIds.delete(emailAddress)
-      } else {
-        // Could potentially use output binding for new users...?
-        usersCreated.push(emailAddress)
-        await usersContainer.items.create(user)
-      }
-    }
-
-    // TODO: refactor to function
-    for (const emailAddress of existingUserIds) {
-      usersInactive.push(emailAddress)
-      const { resource } = await usersContainer.item(emailAddress, emailAddress).read()
-      resource.active = false
-      await usersContainer.item(emailAddress, emailAddress).replace(resource)
-    }
+    const usersInactive = await setUsersInactive(usersContainer, existingUserIds)
 
     context.log(`${usersCreated.length} user(s) created: ${usersCreated}.`)
     context.log(`${usersUpdated.length} user(s) updated: ${usersUpdated}.`)
