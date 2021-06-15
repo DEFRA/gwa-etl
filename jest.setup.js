8ecc081502d2da0e6b31f28e0d0f14eccc7b18d1
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-06-15 16:29:41 +0100

    refactor: rename for hypen consistency

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
-const envVars = require('./test/testEnvVars')
+const envVars = require('./test/test-env-vars')
 
 process.env.AzureWebJobsStorage = envVars.AzureWebJobsStorage
 process.env.AAD_CLIENT_ID = envVars.AAD_CLIENT_ID
