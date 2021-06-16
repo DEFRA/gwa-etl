9356153ca1c173b2c729c2a518205917c7babe01
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-06-16 10:56:28 +0100

    feat: implement combining of all user data

--- CombineDataSources/index.js
+++ CombineDataSources/index.js
@@ -1,35 +1,63 @@
-const Joi = require('joi')
+const { ContainerClient } = require('@azure/storage-blob')
+const getContainerContents = require('../lib/get-container-contents')
+const validateUsers = require('../lib/validate-users')
 
-const schema = Joi.object({
-  id: Joi.string().guid().required(),
-  emailAddress: Joi.string().email().required(),
-  officeCode: Joi.string().pattern(/^[A-Z]{3}:[a-zA-Z0-9-]+$/).required(),
-  officeLocation: Joi.string().required(),
-  orgCode: Joi.string().required(),
-  orgName: Joi.string().required(),
-  givenName: Joi.string().required(),
-  surname: Joi.string().required(),
-  phoneNumbers: Joi.array().items(Joi.string().pattern(/^\+447[0-9]{9}$/)).required()
-})
+const connectionString = process.env.AzureWebJobsStorage
+const dataSourcesContainer = process.env.DATA_SOURCES_CONTAINER
+const containerClient = new ContainerClient(connectionString, dataSourcesContainer)
 
 module.exports = async function (context) {
   try {
+    const { blobTrigger, userDataBlobName } = context.bindingData
     const { blobContents } = context.bindings
+    context.log(`Combine Data Sources Blob Trigger function activated:\n - Blob: ${blobTrigger}\n - Name: ${userDataBlobName}\n - Size: ${blobContents.length} Bytes`)
 
-    const users = JSON.parse(blobContents)
+    const values = await getContainerContents(containerClient)
+    const internalUsers = [values.splice(0, 1)[0] ?? []].flat()
+    const internalUserMap = new Map(internalUsers.map(user => [user.emailAddress, user]))
 
-    const errorUsers = []
-    const validUsers = users.filter(user => {
-      const { error } = schema.validate(user)
-      if (error) {
-        errorUsers.push({ error })
-        return false
+    // Check for duplicates against internalUsers
+    const internalDuplicateUsers = []
+    const nonInternalUsers = values
+      .filter(x => x)
+      .flat()
+      .filter(user => {
+        if (internalUserMap.has(user.emailAddress)) {
+          internalDuplicateUsers.push(user)
+          return false
+        }
+        return true
+      })
+
+    // Find duplicate users within non internal
+    const nonInternalUserMap = new Map()
+    nonInternalUsers.forEach(user => {
+      const emailAddress = user.emailAddress
+      const emailCount = nonInternalUserMap.get(emailAddress)
+      if (emailCount?.count) {
+        emailCount.count++
+      } else {
+        nonInternalUserMap.set(emailAddress, { user, count: 1 })
       }
-      return true
     })
+
+    // Remove duplicates
+    const nonInternalDuplicateUsers = []
+    const nonInternalNonDuplicateUsers = [...nonInternalUserMap.values()].filter(({ count, user }) => {
+      if (count === 1) {
+        return true
+      }
+      nonInternalDuplicateUsers.push(user)
+      return false
+    })
+
+    const users = internalUsers.concat(nonInternalNonDuplicateUsers)
+
+    const { errorUsers, validUsers } = validateUsers(users)
 
     context.bindings.validUsers = validUsers
     context.bindings.errorUsers = errorUsers
+    context.bindings.duplicateUsers = internalDuplicateUsers.concat(nonInternalDuplicateUsers)
   } catch (e) {
     context.log.error(e)
     // Throwing an error ensures the built-in retry will kick in
